075aa6ca322c832c60a0113d9ddfe661d8d17d74
Weslley S. Patrocinio <wespatrocinio@gmail.com>
2018-02-16 15:46:39 -0200

    Fixing typo

--- sample.py
+++ sample.py
@@ -50,6 +50,6 @@ if __name__ == '__main__':
     model.test()
 
 
-    input_text = "Close your eyes and I kiss you. Tomorrow I'll kiss you"
+    input_text = "Close your eyes and I kiss you. Tomorrow I'll miss you"
     prediction = model.predict([input_text])
     print(prediction)
